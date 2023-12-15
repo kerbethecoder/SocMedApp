994fd3cd65cd38a49d78079d128e009171e7b2fd
kerbethecoder <krbycnts@gmail.com>
2023-12-15 20:33:00 +0800

    initialized shadcn; test for shadcn components; changes on authlayout and signup form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { Routes, Route } from 'react-router-dom';
+
+import AuthLayout from './_auth/AuthLayout';
+import SignupForm from './_auth/forms/SignupForm';
+import SigninForm from './_auth/forms/SigninForm';
+import RootLayout from './_root/RootLayout';
+import { Home } from './_root/pages';
+import './globals.css';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
